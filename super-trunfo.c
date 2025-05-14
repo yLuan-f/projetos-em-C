5e9db32c009a803aeb3a2f51ba72a767ba9e9fe1
yLuan-f <yluanfarias2006@gmail.com>
2025-05-14 09:44:38 -0300

    Update super-trunfo.c

--- super-trunfo.c
+++ super-trunfo.c
@@ -1,1 +1,17 @@
+#include <stdio>
 
+int main() {
+  char Estado;
+  char[5] Codigo da Carta;
+  char[15] (string);
+  char Nome da cidade;
+  int Populacao;
+  float Area;
+  float PIB;
+  int Pontos turisticos;
+
+  printf("Carta 1.")
+  printf("Qual o seu estado? \n") 
+    scanf("%d" &Estado) 
+  
+}
